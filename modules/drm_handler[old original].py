285af92fdd99f0b7681f7b02eb3702d72a1f255c
Demoxon <155176751+demoxon@users.noreply.github.com>
2026-07-12 19:01:54 +0530

    Add support for additional video sources in cmd

--- modules/drm_handler[old original].py
+++ modules/drm_handler[old original].py
@@ -360,6 +360,14 @@ async def drm_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp --cookies youtube_cookies.txt -f "{ytf}" "{url}" -o "{name}".mp4'
             else:
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
+            if "jw-prod" in url:
+                cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
+            elif "webvideos.classplusapp." in url:
+               cmd = f'yt-dlp --add-header "referer:https://web.classplusapp.com/" --add-header "x-cdn-tag:empty" -f "{ytf}" "{url}" -o "{name}.mp4"'
+            elif "youtube.com" in url or "youtu.be" in url:
+                cmd = f'yt-dlp --cookies youtube_cookies.txt -f "{ytf}" "{url}" -o "{name}".mp4'
+            else:
+                cmd = f'yt-dlp --referer "https://vigyanvariksh.classx.co.in/" -f "{ytf}" "{url}" -o "{name}.mp4"'    
 #........................................................................................................................................................................................
             try:
                 if m.text:
